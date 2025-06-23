935f212c8a9f948290742891189fc504645ca98a
shota-nagato <shota.nagato@gmail.com>
2025-06-24 01:18:25 +0900

    selenium-webdriverを使うように変更

--- spec/system/static_spec.rb
+++ spec/system/static_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Static", type: :system do
   describe "トップページ" do
-    it "トップページを表示できる" do
+    it "トップページを表示できる", js: true do
       visit root_path
       expect(page).to have_current_path(root_path)
     end

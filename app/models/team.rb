916690918bc00c5d828592fcf20b7302fe574bb1
shota-nagato <shota.nagato@gmail.com>
2025-07-01 20:39:09 +0900

    Teamモデル作成

---
+++ app/models/team.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  owner_id   :bigint           not null
+#
+# Indexes
+#
+#  index_teams_on_owner_id  (owner_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (owner_id => users.id)
+#
+class Team < ApplicationRecord
+  belongs_to :owner, class_name: "User"
+
+  validates :name, presence: true
+end

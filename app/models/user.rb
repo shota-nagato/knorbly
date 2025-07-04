6615a0d8a4812714d525497a08bc00072b465943
shota-nagato <shota.nagato@gmail.com>
2025-07-04 20:53:51 +0900

    更新時にパスワードのバリデーションを回避

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 
   normalizes :email, with: ->(e) { e.strip.downcase }
 

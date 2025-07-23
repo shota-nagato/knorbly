06958d61d4142e2e5796b19b2852ed2d8fceebf7
shota-nagato <shota.nagato@gmail.com>
2025-07-24 00:07:40 +0900

    folderのrspec追加

--- spec/system/folders_spec.rb
+++ spec/system/folders_spec.rb
@@ -42,4 +42,48 @@ RSpec.describe "Folders", type: :system do
       end
     end
   end
+
+  describe "編集" do
+    let(:folder) { create(:folder, name: "編集前のフォルダ", team: user.teams.last) }
+
+    it "フォルダを編集できる" do
+      visit folder_path(folder)
+
+      expect(page).to have_content "編集前のフォルダ"
+
+      find("#dropdown-button").click
+
+      expect(page).to have_content "編集"
+
+      click_on "編集"
+
+      expect(page).to have_content "フォルダを編集"
+
+      fill_in "folder_name", with: "編集後のフォルダ"
+
+      expect do
+        click_on "更新する"
+
+        expect(page).to have_content "フォルダを更新しました"
+      end.to change { folder.reload.name }.from("編集前のフォルダ").to("編集後のフォルダ")
+    end
+  end
+
+  describe "削除" do
+    let(:folder) { create(:folder, team: user.teams.last) }
+
+    it "フォルダを削除できる" do
+      visit folder_path(folder)
+
+      find("#dropdown-button").click
+
+      expect(page).to have_content "削除"
+
+      expect do
+        click_on "削除"
+
+        expect(page).to have_content "フォルダを削除しました"
+      end.to change(user.teams.last.folders, :count).by(-1)
+    end
+  end
 end
